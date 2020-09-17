99fe0b9cdfc4939331c6a1ed391cf7aac0d8e0ce
faed <327399770@qq.com>
2020-09-17 09:32:35 +0800

    <br>

--- src/middleware/RecordReturn.php
+++ src/middleware/RecordReturn.php
@@ -3,17 +3,25 @@
 
 namespace Faed\Doc\middleware;
 use Closure;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 class RecordReturn
 {
     public function handle($request, Closure $next)
     {
         /**@var $request Request **/
         $resource = $next($request);
-        /**@var $resource JsonResponse **/
-        Cache::set($request->path(),$resource->getContent());
+        try {
+            /**@var $resource JsonResponse **/
+            Cache::set($request->path(),$resource->getContent());
+        }catch (Exception $exception){
+            Log::error("保存返回缓存:".$exception->getMessage());
+        }
+
         return $resource;
     }
 }
